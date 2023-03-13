cc119e4eb6c16478fad6c3c833ea2d9b9931e1f5
Quynh Ngo <QuynhTNgo@live.com>
2023-03-13 11:28:57 -0600

    Start creating variables for flightmanager.cs

---
+++ Models/Flight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.Models
+{
+    internal class Flight
+    {
+        private string flightCode;
+        private string airlineName;
+        private string originAirport;
+        private string destinationAirport;
+        private string day;
+    }
+}
--- Models/FlightManager.cs
+++ Models/FlightManager.cs
@@ -8,7 +8,9 @@ namespace Assignment2.Models
 {
 	internal class FlightManager
 	{
-		public FlightManager() 
+        private string USERS_TEXT = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Files", "flights.csv");
+        public static List<Airport> users = new List<Airport>();
+        public FlightManager() 
 		{
 		}
 
