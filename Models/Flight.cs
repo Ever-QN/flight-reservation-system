d123be9874cc949725675f1e6a3986ad566463d0
Quynh Ngo <QuynhTNgo@live.com>
2023-03-16 23:42:07 -0600

    Remove GetFlights()

--- Models/Flight.cs
+++ Models/Flight.cs
@@ -42,7 +42,7 @@ namespace Assignment2.Models
 
 		public override string ToString()
 		{
-            return string.Format("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}", FlightCode, AirlineName, OriginAirport, DestinationAirport, Day, Time, Seats, CostPerSeat);
+            return string.Format("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}{8}", FlightCode, AirlineName, OriginAirport, DestinationAirport, Day, Time, Seats, CostPerSeat, Environment.NewLine);
 		}
 	}
 }
